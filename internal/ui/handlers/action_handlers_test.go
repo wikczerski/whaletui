023e99e7d97542bbc07834caa302fa2e09184f99
allahaka <22891046+wikczerski@users.noreply.github.com>
2025-08-19 12:49:17 +0200

    Clean up test file comments and remove unused test code

--- internal/ui/handlers/action_handlers_test.go
+++ internal/ui/handlers/action_handlers_test.go
@@ -17,15 +17,7 @@ func TestNewActionHandlers_InitialState(t *testing.T) {
 	mockUI := mocks.NewMockUIInterface(t)
 	handlers := NewActionHandlers(mockUI)
 	assert.NotNil(t, handlers)
-	// Note: execCommandParser is not a public field, so we can't test it directly
 }
-
-// Note: All ParseExecCommand tests have been removed because:
-// 1. The parseExecCommand method is private
-// 2. The method cannot be tested directly from outside the package
-// 3. To test this functionality, either:
-//    - Make the method public, or
-//    - Test it indirectly through public methods that use it
 
 func TestActionHandlers_Constructor(t *testing.T) {
 	mockUI := mocks.NewMockUIInterface(t)
